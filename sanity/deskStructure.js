54c52900015d1d02ee93cd4b8bd464998087f33e
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-09-14 11:23:19 +0200

    utkast till schemastruktur
    snälla patrik kolla på detta vi fattar inte

--- sanity/deskStructure.js
+++ sanity/deskStructure.js
@@ -1,16 +1,46 @@
-import S from '@sanity/desk-tool/structure-builder';
+import S from "@sanity/desk-tool/structure-builder";
 
 export default () =>
   S.list()
-    .title('Innehåll')
+    .title("Innehåll")
     .items([
       S.listItem()
-        .title('Företagsinfo')
+        .title("Företagsinfo")
         .child(
-          S.document().schemaType('companyInfo').documentId('companyInfo')
+          S.document().schemaType("companyInfo").documentId("companyInfo")
         ),
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => !['companyInfo'].includes(item.getId())
+        (item) => !["companyInfo"].includes(item.getId())
       ),
+      S.listItem()
+        .title("Om")
+        .child(
+          S.list()
+            .title("Om info ")
+            .items([
+              S.listItem().title("Om cafeét").schemaType("basicInfoSection"),
+              S.listItem()
+                .title("Om bokhandeln")
+                .schemaType("basicInfoSection"),
+              S.listItem()
+                .title("Om barnavdelningen")
+                .schemaType("basicInfoSection"),
+              S.listItem()
+                .title("Om författarkvällar mot författare")
+                .schemaType("basicInfoSection"),
+              S.listItem()
+                .title("Om författarkvällar mot kunder")
+                .schemaType("basicInfoSection"),
+              S.listItem()
+                .title("Om bokcirklar")
+                .schemaType("basicInfoSection"),
+              S.listItem()
+                .title("Om bokhandelskvällar")
+                .schemaType("basicInfoSection")
+                .child(
+                  S.documentTypeList("basicInfoSection").title("Information")
+                ),
+            ])
+        ),
     ]);
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -1,21 +1,22 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator';
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type';
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // We import object and document schemas
-import blockContent from './objects/blockContent';
-import category from './documents/category';
-import post from './documents/post';
-import author from './documents/author';
-import book from './documents/book';
-import companyInfo from './documents/companyInfo';
+import blockContent from "./objects/blockContent";
+import category from "./documents/category";
+import post from "./documents/post";
+import author from "./documents/author";
+import book from "./documents/book";
+import companyInfo from "./documents/companyInfo";
+import basicInfoSection from "./documents/basicInfoSection";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
@@ -26,6 +27,7 @@ export default createSchema({
     book,
     companyInfo,
     category,
+    basicInfoSection,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
