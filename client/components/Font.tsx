1101b5736f45750da74a700aa8bd53e0f5ee20e3
h-emma <emmah92@hotmail.com>
2022-09-15 12:54:28 +0200

    Add fonts, do not work

---
+++ client/components/Font.tsx
@@ -0,0 +1,8 @@
+import { css } from 'styled-components'
+
+export const FontCalibri = css`
+  @font-face {
+    font-family: 'Calibri';
+    src: url('../public/assets/fonts/calibri-regular.woff') format('woff');
+  }
+`
