433069c6a6f38150766bb2c90690e39e5cf7a5a6
patrikstaaf <staaf.patrik@gmail.com>
2022-09-20 22:08:31 +0200

    Update sanity document types

--- client/types.d.ts
+++ client/types.d.ts
@@ -1,4 +1,4 @@
-export interface CalendarData {
+export interface Calendar {
   _createdAt: string
   _id: string
   _rev: string
@@ -11,47 +11,139 @@ export interface CalendarData {
   event: string
 }
 
-export interface StartPageSanityData {
-  companyInfo: {
-    address: string
-    email: string
-    facebookUrl: string
-    heroText: string
-    instagramUrl: string
-    openingHours: string
-    phone: string
-  }
-  bookOfTheMonth: {
-    cover: string
-    coverAlt: string
-    author: string
-    description: string
-    genre: string
-    releaseYear: string
-    title: string
-  }
-  aboutTheStore: {
-    shortGeneralDescriptionAboutTheStore: string
-    generalImage: string
-    generalImageAlt: string
-    shortDescriptionAboutTheCafe: string
-    cafeImage: string
-    cafeImageAlt: string
-  }
-  permanentOffer: {
-    title: string
-    price: string
-    image: string
-    imageAlt: string
-  }
-  aboutTheEvents: {
-    descriptionHostingAuthorNights: string
-    shortGeneralDescriptionAboutBookClub: string
-    generalImageAuthorBookClub: string
-    generalImageAuthorBookClubAlt: string
-    generalImageAuthorNights: string
-    generalImageAuthorNightsAlt: string
-  }
+export interface CompanyInfo {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+  address: string
+  closestPublicTransportStop: string
+  companyName: string
+  directions: string
+  email: string
+  facebookUrl: string
+  heroText: string
+  instagramUrl: string
+  openingHours: string
+  phone: string
+}
 
-  calendar: CalendarData[]
+export interface BookOfTheMonth {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+  author: string
+  description: string
+  genre: string
+  releaseYear: string
+  title: string
+  cover: {
+    _type: string
+    alt: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }
+}
+
+export interface AboutTheStore {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+  cafeImage: {
+    _type: string
+    alt: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }
+  descriptionAboutTheBookStore: string
+  descriptionAboutTheCafe: string
+  descriptionAboutTheChildSection: string
+  generalImage: {
+    _type: string
+    alt: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }
+  imagesOfChildSection: {
+    _key: string
+    _type: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }[]
+  shortDescriptionAboutTheCafe: string
+  shortGeneralDescriptionAboutTheStore: string
+}
+
+export interface AboutTheEvents {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+  generalImageAuthorBookClub: {
+    _type: string
+    alt: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }
+  generalImageAuthorNights: {
+    _type: string
+    alt: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }
+  imagesOfPreviousAuthorNights: {
+    _key: string
+    _type: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }[]
+  shortGeneralDescriptionAboutBookClub: string
+  shortGeneralDescriptionAboutTheStore: string
+}
+
+export interface PermanentOffer {
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+  image: {
+    _type: string
+    alt: string
+    asset: {
+      _ref: string
+      _type: string
+    }
+  }
+  price: string
+  title: string
+}
+
+export interface StartPageSanityData {
+  companyInfo: CompanyInfo
+  bookOfTheMonth: BookOfTheMonth
+  aboutTheStore: AboutTheStore
+  aboutTheEvents: AboutTheEvents
+  permanentOffer: PermanentOffer
+  calendar: Calendar[]
 }
