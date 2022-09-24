cd22ba4ddb1ff8ac332121f918a338d4b6b1a6a3
patrikstaaf <staaf.patrik@gmail.com>
2022-09-24 13:07:45 +0200

    Add 404 page

---
+++ client/pages/404.tsx
@@ -0,0 +1,35 @@
+import { NextPage } from 'next'
+import Link from 'next/link'
+import styled from 'styled-components'
+import Layout from '../components/Layout'
+import { Text, H1 } from '../components/Text'
+
+const Section = styled.section`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const Underline = styled.span`
+  text-decoration: underline;
+`
+
+const NotFound: NextPage = () => {
+  return (
+    <Layout
+      title="Majornas Böcker och Kaffe"
+      description="Tyvärr finns ej sidan du försöker hitta"
+    >
+      <Section>
+        <H1 Color={true}>Oh no, sidan finns inte...</H1>
+        <Text Color={true}>
+          Gå tillbaka till <Link href="/">startsidan</Link>.
+        </Text>
+      </Section>
+    </Layout>
+  )
+}
+
+export default NotFound
